52cf7f08f097602dada013a507119fa7362ffae8
JunhoKim <bagho1999@gmail.com>
2025-01-21 17:49:29 +0900

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -18,14 +18,14 @@ const initializeServer = async () => {
     console.log('Database connected. Starting server');
 
     app.listen(PORT, () => {
-      console.log(`Server is running on http://dev-mate.glitch.me`);
+      console.log(`Server is running on http://localhost:${PORT}`);
     });
   } catch (error) {
     console.error('Failed to start server : ', error);
   }
 };
-app.use('/comments', commentRoutes);
 
+app.use('/comments', commentRoutes);
 app.use('/boards/:boardId/posts', postRoute);
 app.use('/api/users', userRoutes);
 
